128ca90488b345563a124a29185b218aa47203cb
bbicee <152702438+bbicee@users.noreply.github.com>
2024-10-09 07:20:10 +0700

    Delete Buoi8 directory

--- Buoi8/delete_role.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'roles.php';
-
-if (isset($_POST['delete'])) {
-    $id = $_POST['id'];
-    delete_role($id);
-    header("Location: roles_list.php"); // Chuyển hướng sau khi xóa
-}
--- Buoi8/department_delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'department.php';
-
-if (isset($_POST['delete'])) {
-    $id = $_POST['id'];
-    delete_department($id);
-    header("Location: department_list.php"); // Chuyển hướng sau khi xóa
-}
